17f81dcca734550a2f3b88f5c1206627540593ba
kw22228 <hot5521@naver.com>
2022-06-20 17:56:59 +0900

    add: global styled

---
+++ framer-motion/src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+
+    @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+    
+    html{
+        box-sizing: inherit;
+    }
+
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+
+    body{
+        font-family: 'Source Sans Pro', sans-serif;
+        color: black;
+        background:linear-gradient(135deg,#e09,#d0e);
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+
+export default GlobalStyle;
