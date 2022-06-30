cc554d342ba84cc4e21c25bdf71ee2dfa15ff36b
김재원 <kw2282222@gmail.com>
2022-06-30 21:38:58 +0900

    init: main page

---
+++ GSAP/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from '../components/About/About';
+import Home from '../components/Home/Home';
+
+const MainPage = () => {
+    return (
+        <>
+            <Home />
+            <About />
+        </>
+    );
+};
+
+export default MainPage;
