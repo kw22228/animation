b7c2b3f93ea75f6a3ff8ebe4fbe69ae79196ae62
김재원 <kw2282222@gmail.com>
2022-07-02 00:01:22 +0900

    add: Loader

--- GSAP/src/App.js
+++ GSAP/src/App.js
@@ -1,17 +1,27 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
 
 import MainPage from './pages/MainPage';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollTriggerProxy from './animation/gsap/ScrollTriggerProxy';
+import Loader from './components/Loader/Loader';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
     const containRef = useRef(null);
 
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true);
+        }, 3000);
+    }, []);
     return (
         <>
             <BrowserRouter>
+                <AnimatePresence>{!loaded && <Loader />}</AnimatePresence>
                 <LocomotiveScrollProvider //
                     options={{
                         smooth: true,
