a4a86340a9a296dad31d97fe5d7267a7f1d82bca
김재원 <kw2282222@gmail.com>
2022-07-01 23:17:31 +0900

    add: scroll handler

--- GSAP/src/pages/MainPage.jsx
+++ GSAP/src/pages/MainPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import About from '../components/About/About';
+import Banner from '../components/Banner/Banner';
+import Footer from '../components/Footer/Footer';
 import Home from '../components/Home/Home';
+import NewArrival from '../components/NewArraval/NewArrival';
+import Shop from '../components/Shop/Shop';
 
 const MainPage = () => {
     return (
         <>
             <Home />
             <About />
+            <Shop />
+            <Banner />
+            <NewArrival />
+            <Footer />
         </>
     );
 };
