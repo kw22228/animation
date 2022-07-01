1cb1679dd565a5e4cc13c88dc11be088d43ad1aa
김재원 <kw2282222@gmail.com>
2022-07-01 23:14:34 +0900

    add: image total files

---
+++ GSAP/src/assets/Images/newArrival.js
@@ -0,0 +1,12 @@
+import img11 from './11.webp';
+import img12 from './12.webp';
+import img13 from './13.webp';
+import img14 from './14.webp';
+
+export default [
+    //
+    { image: img11, title: 'Denim' },
+    { image: img12, title: 'cool Dresses' },
+    { image: img13, title: 'Jackets' },
+    { image: img14, title: 'T-shirts' },
+];
---
+++ GSAP/src/assets/Images/shop.js
@@ -0,0 +1,23 @@
+import img1 from './1.webp';
+import img2 from './2.webp';
+import img3 from './3.webp';
+import img4 from './4.webp';
+import img5 from './5.webp';
+import img6 from './6.webp';
+import img7 from './7.webp';
+import img8 from './8.webp';
+import img9 from './9.webp';
+import img10 from './10.webp';
+
+export default [
+    { image: img1, title: 'Man Basics' },
+    { image: img2, title: 'Tops' },
+    { image: img3, title: 'Sweatshirts' },
+    { image: img4, title: 'Ethnic Wear' },
+    { image: img5, title: 'Blazers' },
+    { image: img6, title: 'Suits' },
+    { image: img7, title: 'Antiques' },
+    { image: img8, title: 'Jewellery' },
+    { image: img9, title: 'Watches' },
+    { image: img10, title: 'Special Edition' },
+];
