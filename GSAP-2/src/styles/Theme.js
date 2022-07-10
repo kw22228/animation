b43c0c35ffa76cd2b3c31f4050cc5957b5c546a5
김재원 <kw2282222@gmail.com>
2022-07-10 23:49:38 +0900

    init: global styles

---
+++ GSAP-2/src/styles/Theme.js
@@ -0,0 +1,11 @@
+const Theme = {
+    background: '#eff7f9',
+    black: '#0a0b10',
+    purple: '#803bec',
+    pink: '#e5a1f8',
+    white: '#fff',
+    nav: '#35353f',
+    nav2: '#3f3d56',
+};
+
+export default Theme;
---
+++ GSAP-2/src/styles/variable.js
@@ -0,0 +1,11 @@
+const deviceSize = {
+    tablet: '64em',
+    mobile: '48em',
+    mobileSM: '40em',
+};
+
+export const device = {
+    tablet: `max-width: ${deviceSize.tablet}`,
+    mobile: `max-width: ${deviceSize.mobile}`,
+    mobileSM: `max-width: ${deviceSize.mobileSM}`,
+};
