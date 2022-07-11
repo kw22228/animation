c20a901507d54592b9938cd9eb2556b6b3af86a6
kw22228 <hot5521@naver.com>
2022-07-11 17:38:48 +0900

    update: responsive

--- GSAP-2/src/components/Footer/Footer.jsx
+++ GSAP-2/src/components/Footer/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import * as s from './Footer.style';
 
+import Twitter from '../../assets/twitter-square-brands.svg';
+import Instagram from '../../assets/instagram-square-brands.svg';
+import Gmail from '../../assets/envelope-open-solid.svg';
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <s.Footer>
+            <s.LeftText>&copy; 2022 Built and Design by JW.</s.LeftText>
+            <s.RightText>
+                Reach out to me via😊
+                <a href="#">
+                    <img src={Twitter} alt="Twitter" />
+                </a>
+                <a href="#">
+                    <img src={Instagram} alt="Instagram" />
+                </a>
+                <a href="mailto:kw2282222@gmail.com">
+                    <img src={Gmail} alt="Gmail" />
+                </a>
+            </s.RightText>
+        </s.Footer>
+    );
 };
 
 export default Footer;
--- GSAP-2/src/components/Footer/Footer.style.js
+++ GSAP-2/src/components/Footer/Footer.style.js
@@ -1,1 +1,46 @@
 import styled from 'styled-components';
+import { device } from '../../styles/variable';
+
+export const Footer = styled.section`
+    padding: 1.2rem calc(2.5rem + 2.5vw);
+    font-size: 1rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (${device.mobile}) {
+        flex-direction: column;
+
+        div {
+            &:first-child {
+                margin-bottom: 1rem;
+            }
+        }
+    }
+`;
+
+export const LeftText = styled.div``;
+
+export const RightText = styled.div`
+    display: flex;
+    align-items: center;
+
+    img {
+        width: 1.5rem;
+        height: auto;
+        filter: invert(100%);
+        margin-left: 1rem;
+        transition: all 0.2s ease-in-out;
+    }
+
+    a {
+        &:hover {
+            img {
+                transform: scale(1.5);
+                filter: invert(50%) sepia(100%) saturate(300%) hue-rotate(216deg) brightness(100%)
+                    contrast(97%);
+            }
+        }
+    }
+`;
