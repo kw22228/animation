c6e1f9f4fcec3df53664927bd880a668a98ad93a
kw22228 <hot5521@naver.com>
2022-07-11 17:29:34 +0900

    add: scroll trigger

--- GSAP-2/src/components/ScrollToTop/ScrollToTop.jsx
+++ GSAP-2/src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import * as s from './ScrollToTop.style';
+import handleScrollUp from '../../utils/handleScrollUp';
+
+import Arrow from '../../assets/arrow-up.svg';
 
 const ScrollToTop = () => {
-    return <div>ScrollToTop</div>;
+    return (
+        <s.Up onClick={() => handleScrollUp('#home')}>
+            <img src={Arrow} alt="To top" />
+        </s.Up>
+    );
 };
 
 export default ScrollToTop;
